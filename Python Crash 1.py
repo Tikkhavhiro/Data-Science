ecac3fd55e2fbf47e8e5e2db50c69613bd96abff
Arditya Dwifians <107258341+Tikkhavhiro@users.noreply.github.com>
2023-05-30 19:16:59 +0700

    Add files via upload

---
+++ Python Crash 1.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+1
+
+
+# In[2]:
+
+
+1.0
+
+
+# In[3]:
+
+
+2+2
+
+
+# In[4]:
+
+
+2*2
+
+
+# In[5]:
+
+
+2/4
+
+
+# In[7]:
+
+
+2**5
+
+
+# In[8]:
+
+
+2+3*4-1
+
+
+# In[9]:
+
+
+(3+1)*(2*2)
+
+
+# In[10]:
+
+
+4%2
+
+
+# In[11]:
+
+
+5%2
+
+
+# In[12]:
+
+
+70%9
+
+
+# In[13]:
+
+
+6%4
+
+
+# In[14]:
+
+
+var = 2
+
+
+# In[15]:
+
+
+var
+
+
+# In[16]:
+
+
+y=3
+
+z = var * y
+
+
+# In[18]:
+
+
+print(z)
+
+
+# In[19]:
+
+
+z
+
+
+# In[22]:
+
+
+total_var = z
+
+
+# In[23]:
+
+
+total_var
+
+
+# In[24]:
+
+
+#comment
+
+
+# In[25]:
+
+
+'single quote'
+
+
+# In[26]:
+
+
+"for string"
+
+
+# In[27]:
+
+
+num = 20
+name = "Dii"
+
+
+# In[31]:
+
+
+z = 'My name is {}, and my age is {}'.format(name, num)
+
+
+# In[33]:
+
+
+print(z)
+
+
+# In[34]:
+
+
+'My name is {one}, and my age is {two}, and {one}'.format(one=name, two=num)
+
+
+# In[37]:
+
+
+s = 'hello'
+s[1:]
+
+
+# In[39]:
+
+
+s[:3]
+
+
+# In[40]:
+
+
+s[:]
+
+
+# In[45]:
+
+
+s[1:4]
+
+
+# In[46]:
+
+
+[1,2,3]
+
+
+# In[47]:
+
+
+['a','b','c']
+
+
+# In[48]:
+
+
+my_list = ['a','b','c']
+
+
+# In[49]:
+
+
+my_list
+
+
+# In[50]:
+
+
+my_list[2]
+
+
+# In[52]:
+
+
+my_list[0] = 'Hi'
+
+
+# In[53]:
+
+
+my_list
+
+
+# In[66]:
+
+
+nest = [1,2,3,4, [5,6,7]]
+
+
+# In[67]:
+
+
+nest
+
+
+# In[74]:
+
+
+nest[4]
+
+
+# In[73]:
+
+
+nest[4][2]
+
+
+# In[75]:
+
+
+ver = [1,2,3,4,[5,6,7,8,['target']]]
+
+
+# In[76]:
+
+
+ver
+
+
+# In[77]:
+
+
+ver[4]
+
+
+# In[78]:
+
+
+ver[4][4]
+
+
+# In[82]:
+
+
+ver[4][4][0]
+
+
+# In[ ]:
+
+
+
+
